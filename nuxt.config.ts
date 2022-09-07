ea9c89211ef6e20ab8ddbd8626d792cc291ac398
imStav <saprocontact@gmail.com>
2022-09-07 21:08:19 +0200

    Added content theme highlighting in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,12 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content']
+  modules: ['@nuxt/content'],
+
+  // Syntax / code highlighting
+  content: {
+    highlight: {
+      theme: 'github-light'
+    }
+  }
 })
