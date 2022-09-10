6694039710b6b05c8849f77e0bbe7b6f261d69a9
imStav <saprocontact@gmail.com>
2022-09-10 11:46:46 +0200

    Added Tailwind CSS to the project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content'],
+  modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
 
   // Syntax / code highlighting
   content: {
