4953c35e0aa0a22d8471ee3cf9302724dfadfde5
imStav <saprocontact@gmail.com>
2022-09-12 20:53:34 +0200

    Trying to work on Spans Nuxt Content feature

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{html,js,vue,ts}'
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',
   ],
   theme: {
     extend: {},
