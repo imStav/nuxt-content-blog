b1c65607e1d01d975ae2428f50db41dcb597df03
imStav <saprocontact@gmail.com>
2022-09-15 15:05:47 +0200

    Resolved Spans issue
    Added markdown files in the tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './content/**/*.md',
     './components/**/*.{html,js}',
     './pages/**/*.{html,js}',
     './index.html',
