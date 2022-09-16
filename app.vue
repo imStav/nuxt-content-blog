f4be72be125cb384f3d084ae52f60a8170c3c6fa
imStav <saprocontact@gmail.com>
2022-09-16 19:12:13 +0200

    Added navigation menu

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+    <div class="flex justify-center border border-b">
+      <ContentNavigation v-slot="{ navigation }">
+        <ul>
+          <li v-for="link of navigation" :key="link._path">
+            <NuxtLink :to="link._path">{{ link.title }}</NuxtLink>
+          </li>
+        </ul>
+      </ContentNavigation>
+    </div>
+
     <NuxtPage />
   </div>
 </template>
