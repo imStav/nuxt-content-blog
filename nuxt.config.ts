2633aa161051d9269b747fc0bff97ed7b915d15c
imStav <saprocontact@gmail.com>
2022-09-18 21:35:16 +0200

    Removed code syntax highlighting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,4 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
-
-  // Syntax / code highlighting
-  content: {
-    highlight: {
-      theme: 'github-light'
-    }
-  }
 })
