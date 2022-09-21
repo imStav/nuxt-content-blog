73edcb7b493d90c2dbd8403838ed11f99a7d48d2
imStav <saprocontact@gmail.com>
2022-09-21 20:32:48 +0200

    Working on Blog layout
    + added a template article

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,9 +1,9 @@
 <template>
-    <main>
+    <main class="h-[calc(100vh-3.9rem)] grid gap-10 place-content-center p-16 md:grid-cols-2 xl:grid-cols-3 xl:px-96 bg-slate-200 dark:bg-black dark:text-slate-400">
       <ContentList path="/blog" v-slot="{ list }">
-        <div v-for="article in list" :key="article._path">
-          <h2>{{ article.title }}</h2>
-          <p>{{ article.description }}</p>
+        <div v-for="article in list" :key="article._path" class="border border-slate-500 h-fit p-4">
+          <h2 class="text-2xl font-bold tracking-wide">{{ article.title }}</h2>
+          <p class="leading-tight">{{ article.description }}</p>
         </div>
       </ContentList>
     </main>
