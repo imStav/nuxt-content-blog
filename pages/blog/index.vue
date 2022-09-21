32c33c12c30a72914a8fb0d56af152086f028144
imStav <saprocontact@gmail.com>
2022-09-21 20:40:51 +0200

    Quick change on the Blog section's height

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <main class="h-[calc(100vh-3.9rem)] grid gap-10 place-content-center p-16 md:grid-cols-2 xl:grid-cols-3 xl:px-96 bg-slate-200 dark:bg-black dark:text-slate-400">
+    <main class="min-h-screen grid gap-10 place-content-center p-16 md:grid-cols-2 xl:grid-cols-3 xl:px-96 bg-slate-200 dark:bg-black dark:text-slate-400">
       <ContentList path="/blog" v-slot="{ list }">
         <div v-for="article in list" :key="article._path" class="border border-slate-500 h-fit p-4">
           <h2 class="text-2xl font-bold tracking-wide">{{ article.title }}</h2>
