9a6928f49df8b9940cf65a7834e10355f68d6f7e
imStav <saprocontact@gmail.com>
2022-09-22 15:03:53 +0200

    Quick fix for the navbar UI

--- app.vue
+++ app.vue
@@ -1,10 +1,9 @@
 <template>
   <div>
-    <nav class="flex justify-center bg-slate-200 dark:bg-black dark:text-slate-400 sticky top-0">
+    <nav class="flex justify-center bg-slate-200 text-slate-700 dark:bg-neutral-900 dark:text-slate-400 sticky top-0">
       <ContentNavigation v-slot="{ navigation }">
         <ul class="flex flex-row">
-          <li 
-            v-for="link of navigation" :key="link._path" class="text-xl mx-2 my-4 dark:hover:text-white">
+          <li v-for="link of navigation" :key="link._path" class="text-xl mx-2 my-4 hover:text-black dark:hover:text-slate-200 duration-200">
             <NuxtLink :to="link._path">{{ link.title }}</NuxtLink>
           </li>
         </ul>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="min-h-screen bg-slate-200 text-slate-800 dark:bg-neutral-900 dark:text-slate-400">
+  <main class="min-h-screen bg-slate-200 text-slate-700 dark:bg-neutral-900 dark:text-slate-400">
     <div class="grid gap-10 place-content-center p-16 md:grid-cols-2 xl:grid-cols-3 xl:px-96">
       <ContentList path="/blog" v-slot="{ list }">
         <div v-for="article in list" :key="article._path" class="bg-slate-100 dark:bg-neutral-800 h-fit px-4 pt-4 pb-6 rounded-lg border border-transparent hover:border-blue-500 duration-200">
