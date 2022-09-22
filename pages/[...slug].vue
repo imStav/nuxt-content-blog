a1c150e936699716050770be34bd3753d5d97596
imStav <saprocontact@gmail.com>
2022-09-22 15:41:04 +0200

    Working on blog articles layout

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,7 @@
 <template>
-  <main>
-    <ContentDoc />
+  <main class="min-h-screen bg-slate-200 text-slate-700 dark:bg-neutral-900 dark:text-slate-400">
+    <div class="p-16 xl:px-96">
+      <ContentDoc />
+    </div>
   </main>
 </template>
