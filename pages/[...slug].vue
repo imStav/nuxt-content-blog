e6b83a8b51f7a0459f48a04048e712614a86f863
imStav <saprocontact@gmail.com>
2022-09-23 16:37:42 +0200

    ContentDoc wrapper width adjustments
    For Tablets and Desktops

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,6 +1,6 @@
 <template>
   <main class="min-h-screen bg-slate-200 text-slate-700 dark:bg-neutral-900 dark:text-slate-400">
-    <div class="p-16 xl:px-96">
+    <div class="p-16 md:w-2/3 xl:w-1/2 mx-auto">
       <ContentDoc v-slot="{ doc }">
         <h1 class="bg-slate-100 dark:bg-neutral-800 w-fit rounded-full px-4 py-1">{{ doc.title }}</h1>
         <ContentRenderer :value="doc" class="my-8" />
