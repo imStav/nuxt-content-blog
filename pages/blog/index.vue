a3bc97311216e3ed5b9bb341d526b708ce8bed55
imStav <saprocontact@gmail.com>
2022-09-24 20:11:42 +0200

    Working on general layout / UI
    Trying to find a better global responsiveness

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="min-h-screen bg-slate-200 text-slate-700 dark:bg-neutral-900 dark:text-slate-400">
-    <div class="grid gap-10 place-content-center p-16 md:grid-cols-2 xl:grid-cols-3 xl:px-96">
+    <div class="grid gap-10 place-content-center p-16 md:grid-cols-2 lg:w-2/3 xl:grid-cols-3 xl:w-2/3 mx-auto">
       <ContentList path="/blog" v-slot="{ list }">
         <div v-for="article in list" :key="article._path" class="bg-slate-100 dark:bg-neutral-800 h-fit px-4 pt-4 pb-6 rounded-lg border border-transparent hover:border-blue-500 duration-200">
           <h2 class="text-2xl font-bold tracking-wide">{{ article.title }}</h2>
