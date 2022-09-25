7e9bdb4fc4fe79f39f8bfa5332f1cbe13345e6de
imStav <saprocontact@gmail.com>
2022-09-25 16:29:07 +0200

    Implementing and templating new data
    Articles will receive new front-matter data such as author, date, etc.

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -3,7 +3,11 @@
     <div class="grid gap-10 place-content-center p-16 md:grid-cols-2 lg:w-2/3 xl:grid-cols-3 xl:w-2/3 mx-auto">
       <ContentList path="/blog" v-slot="{ list }">
         <div v-for="article in list" :key="article._path" class="bg-slate-100 dark:bg-neutral-800 h-fit px-4 pt-4 pb-6 rounded-lg border border-transparent hover:border-blue-500 duration-200">
-          <h2 class="text-2xl font-bold tracking-wide">{{ article.title }}</h2>
+          <h2 class="text-2xl font-bold tracking-wide w-fit">{{ article.title }}</h2>
+          <div class="flex flex-row items-center">
+            <p>{{ article.author }}</p>
+            <p class="mx-4">{{ article.date }}</p>
+          </div>
           <p class="mt-4 mb-8">{{ article.description }}</p>
           <NuxtLink :to="article._path" class="bg-slate-200 dark:bg-neutral-900 hover:bg-blue-500 hover:text-white duration-200 px-5 py-2 rounded-full">Read more</NuxtLink>
         </div>
