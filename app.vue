1ceb8140fb15b610c1af3e1c80cb5f5dbece7de3
imStav <saprocontact@gmail.com>
2022-09-26 19:58:26 +0200

    Added some comments

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <nav class="flex justify-center bg-slate-200 text-slate-700 dark:bg-neutral-900 dark:text-slate-400 sticky top-0">
+
+      <!-- Enables and displays dynamics navigation links -->
       <ContentNavigation v-slot="{ navigation }">
         <ul class="flex flex-row">
           <li v-for="link of navigation" :key="link._path" class="text-xl mx-2 my-4 hover:text-black dark:hover:text-slate-200 duration-200">
@@ -8,6 +10,7 @@
           </li>
         </ul>
       </ContentNavigation>
+
     </nav>
 
     <NuxtPage />
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,6 +1,11 @@
 <template>
   <main class="min-h-screen bg-slate-200 text-slate-700 dark:bg-neutral-900 dark:text-slate-400">
     <div class="grid gap-10 place-content-center p-16 md:grid-cols-2 lg:w-2/3 xl:grid-cols-3 xl:w-2/3 mx-auto">
+
+      <!-- 
+        Generates a card for each article contained in the 'blog' subfolder 
+        (./content/blog) and displays front-matter data (title, date...) 
+      -->
       <ContentList path="/blog" v-slot="{ list }">
         <div v-for="article in list" :key="article._path" class="bg-slate-100 dark:bg-neutral-800 h-fit px-4 pt-4 pb-6 rounded-lg border border-transparent hover:border-blue-500 duration-200">
           <h2 class="text-2xl font-bold tracking-wide w-fit">{{ article.title }}</h2>
@@ -12,6 +17,7 @@
           <NuxtLink :to="article._path" class="bg-slate-200 dark:bg-neutral-900 hover:bg-blue-500 hover:text-white duration-200 px-5 py-2 rounded-full">Read more</NuxtLink>
         </div>
       </ContentList>
+
     </div>
   </main>
 </template>
