d1167af43aed8cf1074c80bc5b18973220d6a634
imStav <saprocontact@gmail.com>
2022-10-01 17:51:02 +0200

    Reworking Banner component

--- components/content/Banner.vue
+++ components/content/Banner.vue
@@ -1,9 +1,5 @@
 <template>
     <div class="bg-slate-100 dark:bg-neutral-800 rounded-lg px-4 py-2">
         <h1 class="text-3xl font-bold"><slot /></h1>
-
-        <div class="underline">
-            <slot name="link" />
-        </div>
     </div>
 </template>
